e1e3993ccd87900f039d4a461a18fa4a1e0da2f3
Samantha Sanford <samanthawebdev@gmail.com>
2020-03-30 00:59:18 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
   # This is the array we will be passing into the remove_strawberry method
   # contacts = {
@@ -13,7 +13,14 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
+  end
 end
